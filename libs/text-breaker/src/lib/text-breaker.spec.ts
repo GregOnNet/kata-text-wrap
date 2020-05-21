0c730c63b60d08677082ffb0b401a082ffdbe1d8
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-05-21 08:35:18 +0200

    chore(dev): install prettier

--- libs/text-breaker/src/lib/text-breaker.spec.ts
+++ libs/text-breaker/src/lib/text-breaker.spec.ts
@@ -1,7 +1,22 @@
 import { textBreaker } from './text-breaker';
 
-describe('textBreaker', () => {
-  it('should work', () => {
-    expect(textBreaker()).toEqual('text-breaker');
+describe('@flow-design/text-breaker', () => {
+  describe('When no text is passed', () => {
+    it.each`
+      noText
+      ${undefined}
+      ${null}
+      ${''}
+    `('yields empty', ({noText}) => {
+const breaker = new LineBreaker();
+const brokenText = breaker.break(noText);
+expect(brokenText).toBe('')
+    });
   });
 });
+
+class LineBreaker {
+  break(text: string|null|undefined): string {
+if (text) { return  ''}
+  }
+}
